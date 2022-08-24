57eccf3ef06dd71e1312308c5841960683e3fe64
Nero Miller <ming927@spp.url.com.tw>
2022-08-24 13:30:25 +0800

    0.6 Profile and avatar

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
 from flask_login import UserMixin
 
@@ -17,6 +18,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -26,6 +29,17 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        """Gravatar generate
+
+        https://www.gravatar.com/avatar/md5-HASH?s=size
+
+        Args:
+            size (int): pixel size from 1 to 2048
+        """
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return "https://www.gravatar.com/avatar/{}?d=robohash&s={}".format(digest, size)
 
 
 class Post(db.Model):
